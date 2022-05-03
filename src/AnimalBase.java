0f3c8b4530b1a8a2b0af5556629a3e1744e1f357
Skandaniel92 <dani32c4@stud.kea.dk>
2022-05-03 09:15:06 +0200

    Test

--- src/AnimalBase.java
+++ src/AnimalBase.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AnimalBase {
 
@@ -9,12 +13,12 @@ public class AnimalBase {
         animals = new ArrayList<>();
     }
 
-    public void start() {
+    public void start() throws FileNotFoundException {
         UserInterface ui = new UserInterface(this);
         ui.start();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         AnimalBase app = new AnimalBase();
         app.start();
     }
@@ -58,12 +62,61 @@ public class AnimalBase {
     }
 
 
-    public void loadDatabase() {
-        System.err.println("LOAD not yet implemented!");
+    public void loadDatabase() throws FileNotFoundException {
+
+        Scanner fileScanner = new Scanner(new File("animal.csv"));
+
+        while (fileScanner.hasNextLine());
+
+        String line = fileScanner.nextLine();
+
+        Scanner lineScanner = new Scanner(line).useDelimiter(";");
+
+        String name = lineScanner.next();
+        String desc = lineScanner.next();
+        String type = lineScanner.next();
+        int age = lineScanner.nextInt();
+        double weight = lineScanner.nextDouble();
+
+
+        Animal animal = new Animal(name, desc, type, age, weight);
+
+        animals.add(animal);
+
+        //System.err.println("LOAD not yet implemented!");
     }
 
-    public void saveDatabase() {
-        System.err.println("SAVE not yet implemented!");
+    public void saveDatabase() throws FileNotFoundException {
+        PrintStream outputfile = new PrintStream("animals.csv");
+
+        for (Animal animal : animals) {
+            writeAnimal(outputfile, animal);
+        }
+        outputfile.close();
+
+    }
+
+    public void writeAnimal(PrintStream outputfile, Animal animal){
+        outputfile.print(animal.getName());
+
+        outputfile.print(";");
+
+        outputfile.print(animal.getDesc());
+
+        outputfile.print(";");
+
+        outputfile.print(animal.getType());
+
+        outputfile.print(";");
+
+        outputfile.print(animal.getAge());
+
+        outputfile.print(";");
+
+        outputfile.print(animal.getWeight());
+
+        outputfile.print("\n");
+
     }
 
 }
